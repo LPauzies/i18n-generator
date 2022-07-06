dba8049aac7722aa0b6a855348541a3316849f77
Lucas Pauzies <lucas.pauzies@capgemini.com>
2022-07-06 16:54:09 +0200

    [FIX] Add comments to functions

--- i18ngenerator/transformer.py
+++ i18ngenerator/transformer.py
@@ -1,15 +1,33 @@
 import unidecode
 import nltk
 
-class Transformer():
+class Transformer:
 
     def __init__(self) -> None:
-        nltk.download("punkt", quiet=True) # To use tokenization
+        """Initialize needed files for ntlk use (grammar, tokenization, ...)"""
+        # Tokenization
+        nltk.download("punkt", quiet=True)
 
     def strip_accents(self, text: str) -> str:
+        """Strip accents in `text` and replace by nearest character
+
+        Args:
+            text (str): The text to strip the accents in
+
+        Returns:
+            str: The accent stripped string
+        """
         return unidecode.unidecode(text)
 
     def capitalize(self, text: str) -> str:
+        """Capitalize each sentence in `text`
+
+        Args:
+            text (str): The text to capitalize sentences in
+
+        Returns:
+            str: The capitalized sentences of the `text`
+        """
         sentences = nltk.tokenize.sent_tokenize(text)
         sentences = list(map(lambda s: f"{s[0].upper()}{s[1:]}", sentences))
         return " ".join(sentences)
