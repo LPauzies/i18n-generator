27e3c3aa0b3826232d02821e76afc12fcc948451
Cair0n <lucas.pauzies@hotmail.fr>
2022-07-07 15:06:03 +0200

    [FIX] Requirements in setup

--- setup.py
+++ setup.py
@@ -1,20 +1,38 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt", "r") as f:
-    requirements = f.readlines()
+# The requirements to build the package
+requirements = [
+    "translators==5.3.1",
+    "unidecode==1.3.4",
+    "nltk"
+]
 
+# Read from Readme.md for long description
+with open("./README.md", "r", encoding="utf-8") as f:
+    readme = f.read() 
+
+# Setup the package
 setup(
-    name="i18n-generator",
-    version="0.0.1",
-    description='Python package for i18n implementation and conversion',
-    author='Lucas Pauzies',
-    author_email='lucas.pauzies@hotmail.fr',
-    url='https://www.python.org/sigs/distutils-sig/',
-    packages=find_packages(
-        where="i18generator", 
+    name = "i18n-generator",
+    version = "0.0.1",
+    author = "Lucas Pauzies",
+    author_email = "lucas.pauzies@hotmail.fr",
+    maintainer = "Lucas Pauzies",
+    maintainer_email = "lucas.pauzies@hotmail.fr",
+    description = "Python package for i18n implementation and conversion",
+    long_description = readme,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/LPauzies/i18n-generator",
+    packages = find_packages(
         exclude=["tests"]
     ),
-    install_requires=requirements,
-    setup_requires=['pytest-runner', 'flake8'],
-    tests_require=['pytest'],
+    install_requires = requirements,
+    setup_requires = ["pytest-runner", "flake8", "wheel"],
+    tests_require = ["pytest"],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = ">=3.8"
 )
