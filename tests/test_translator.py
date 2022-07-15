d69ae2ab5606d1eac85255a90b2da2ded613bd8f
Lucas Pauzies <lucas.pauzies@capgemini.com>
2022-07-15 14:45:35 +0200

    UT

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -7,14 +7,14 @@ class TestTranslator(unittest.TestCase):
 
     def test_translate_text_from_fr(self):
         # Given
-        s_fr = "Il semblerait que Noé et Irène sont à la plage. Ils sont passés par la forêt. C'était hier."
+        s_fr = "Je suis à la plage. Demain, j'irai chez le docteur. Hier, j'étais à l'école."
 
         # Expected
-        s_en = "It seems that Noah and Irene are at the beach. They went through the forest. It was yesterday."
-        s_es = "Parece que Noé e Irene están en la playa. Pasaron por el bosque. Eso fue ayer."
-        s_zh = "看来诺亚和艾琳在海滩上。 他们穿过森林。 那是昨天了。"
-        s_ru = "Кажется, что Ной и Ирен на пляже. Они прошли через лес. Это было вчера."
-        s_ko = "노아와 아이린이 해변에있는 것 같습니다. 그들은 숲을 통과했습니다. 어제였습니다."
+        s_en = "I am at the beach. Tomorrow I will go to the doctor. Yesterday I was in school."
+        s_es = "Estoy en la playa. Mañana iré al médico. Ayer estaba en la escuela."
+        s_zh = "我在海滩。 明天，我将去看医生。 昨天我在学校。"
+        s_ru = "Я на пляже. Завтра пойду к врачу. Вчера я был в школе."
+        s_ko = "나는 해변에있다. 내일, 나는 의사에게 갈 것이다. 어제 나는 학교에 있었다."
 
         # Do
         self.assertEqual(s_en, Translator.translate_text(s_fr, from_language=Language.FRENCH, to_language=Language.ENGLISH))
@@ -25,14 +25,14 @@ class TestTranslator(unittest.TestCase):
 
     def test_translate_text_from_en(self):
         # Given
-        s_en = "It seems that Noah and Irene are at the beach. They went through the forest. It was yesterday."
+        s_en = "I am at the beach. Tomorrow I will go to the doctor. Yesterday I was in school."
 
         # Expected
-        s_fr = "Il semble que Noah et Irene soient à la plage. Ils ont traversé la forêt. C'était hier."
-        s_es = "Parece que Noé e Irene están en la playa. Pasaron por el bosque. Eso fue ayer."
-        s_zh = "看来诺亚和艾琳在海滩上。 他们穿过森林。 那是昨天了。"
-        s_ru = "Кажется, что Ной и Ирен на пляже. Они прошли через лес. Это было вчера."
-        s_ko = "노아와 아이린이 해변에있는 것 같습니다. 그들은 숲을 통과했습니다. 어제였습니다."
+        s_fr = "Je suis à la plage. Demain, j’irai chez le médecin. Hier, j’étais à l’école."
+        s_es = "Estoy en la playa. Mañana iré al médico. Ayer estaba en la escuela."
+        s_zh = "我在海滩。 明天我将去看医生。 昨天我在学校。"
+        s_ru = "Я на пляже. Завтра пойду к врачу. Вчера я был в школе."
+        s_ko = "나는 해변에있다. 내일 나는 의사에게 갈 것이다. 어제 나는 학교에 있었다."
 
         # Do
         self.assertEqual(s_fr, Translator.translate_text(s_en, from_language=Language.ENGLISH, to_language=Language.FRENCH))
@@ -43,14 +43,14 @@ class TestTranslator(unittest.TestCase):
 
     def test_translate_text_from_zh(self):
         # Given
-        s_zh = "看来诺亚和艾琳在海滩上。 他们穿过森林。 那是昨天了。"
+        s_zh = "我在海滩。 明天我将去看医生。 昨天我在学校。"
 
         # Expected
-        s_fr = "Il semble que Noah et Irene soient sur la plage. Ils ont traversé la forêt. C'était hier."
-        s_es = "Parece que Noé e Irene están en la playa. Pasaron por el bosque. Eso fue ayer."
-        s_en = "It seems that Noah and Irene are on the beach. They passed through the forest. That was yesterday."
-        s_ru = "Кажется, что Ной и Ирен на пляже. Они прошли через лес. Это было вчера."
-        s_ko = "노아와 아이린이 해변에있는 것 같습니다. 그들은 숲을 통과했습니다. 어제였습니다."
+        s_fr = "Je suis sur la plage. J'irai chez un médecin demain. J'étais à l'école hier."
+        s_es = "Estoy en la playa. Mañana iré a un médico. Ayer estuve en la escuela."
+        s_en = "I'm on the beach. I will go to a doctor tomorrow. I was at school yesterday."
+        s_ru = "Я на пляже. Завтра пойду к врачу. Я был в школе вчера."
+        s_ko = "나는 해변에있다. 내일 의사에게 갈 게요. 나는 어제 학교에 있었다."
 
         # Do
         self.assertEqual(s_fr, Translator.translate_text(s_zh, from_language=Language.CHINESE, to_language=Language.FRENCH))
@@ -61,14 +61,14 @@ class TestTranslator(unittest.TestCase):
 
     def test_translate_text_from_es(self):
         # Given
-        s_es = "Parece que Noé e Irene están en la playa. Pasaron por el bosque. Eso fue ayer."
+        s_es = "Estoy en la playa. Mañana iré al médico. Ayer estaba en la escuela."
 
         # Expected
-        s_fr = "Il semble que Noah et Irene soient sur la plage. Ils ont traversé la forêt. C'était hier."
-        s_zh = "看来诺亚和艾琳在海滩上。 他们穿过森林。 那是昨天。"        
-        s_en = "It seems that Noah and Irene are on the beach. They passed through the forest. That was yesterday."
-        s_ru = "Кажется, что Ной и Ирен на пляже. Они прошли через лес. Это было вчера."
-        s_ko = "노아와 아이린이 해변에있는 것 같습니다. 그들은 숲을 통과했습니다. 어제였습니다."
+        s_fr = "Je suis sur la plage. Demain, j’irai chez le médecin. Hier, j’étais à l’école."
+        s_zh = "我在海滩。 明天我要去医生办公室。 昨天我在学校。"
+        s_en = "I'm on the beach. Tomorrow I will go to the doctor. Yesterday I was at school."
+        s_ru = "Я на пляже. Завтра пойду к врачу. Вчера я был в школе."
+        s_ko = "나는 해변에있다. 내일 나는 의사의 사무실에 간다. 어제 나는 학교에 있었다."
 
         # Do
         self.assertEqual(s_fr, Translator.translate_text(s_es, from_language=Language.SPANISH, to_language=Language.FRENCH))
@@ -79,14 +79,14 @@ class TestTranslator(unittest.TestCase):
 
     def test_translate_text_from_ru(self):
         # Given
-        s_ru = "Кажется, что Ной и Ирен на пляже. Они прошли через лес. Это было вчера."
+        s_ru = "Я на пляже. Завтра пойду к врачу. Вчера я был в школе."
 
         # Expected
-        s_fr = "Il semble que Noah et Irene sur la plage. Ils ont traversé la forêt. C'était hier."
-        s_zh = "似乎诺亚和艾琳在海滩上。 他们穿过森林。 那是昨天了。"        
-        s_en = "It seems that Noah and Irene on the beach. They went through the forest. It was yesterday."
-        s_es = "Parece que Noé e Irene en la playa. Pasaron por el bosque. Eso fue ayer."        
-        s_ko = "해변에서 노아와 아이린이있는 것 같습니다. 그들은 숲을 통과했습니다. 어제였습니다."
+        s_fr = "Je suis sur la plage. J’irai chez le médecin demain. J’étais à l’école hier."
+        s_zh = "我在海滩上。 我明天去看医生。 昨天我在学校。"
+        s_en = "I'm on the beach. I'll go to the doctor tomorrow. I was at school yesterday."
+        s_es = "Estoy en la playa. Iré al médico mañana. Ayer estuve en la escuela."
+        s_ko = "나는 해변에있다. 내일 의사에게 갈 게요. 어제 나는 학교에 있었다."
 
         # Do
         self.assertEqual(s_fr, Translator.translate_text(s_ru, from_language=Language.RUSSIAN, to_language=Language.FRENCH))
@@ -97,14 +97,14 @@ class TestTranslator(unittest.TestCase):
 
     def test_translate_text_from_ko(self):
         # Given
-        s_ko = "해변에서 노아와 아이린이있는 것 같습니다. 그들은 숲을 통과했습니다. 어제였습니다."
+        s_ko = "나는 해변에있다. 내일 나는 의사에게 갈 것이다. 어제 나는 학교에 있었다."
 
         # Expected
-        s_fr = "Il semble y avoir Noah et Irene sur la plage. Ils ont traversé la forêt. C'était hier."
-        s_zh = "海滩上似乎有诺亚和艾琳。 他们穿过森林。 那是昨天了。"        
-        s_en = "There seems to be Noah and Irene on the beach. They passed through the forest. It was yesterday."
-        s_es = "Parece que hay Noé e Irene en la playa. Pasaron por el bosque. Eso fue ayer."        
-        s_ru = "Кажется, на пляже есть Ной и Ирен. Они прошли через лес. Это было вчера."
+        s_fr = "Je suis sur la plage. Demain, j'irai chez le médecin. Hier, j'étais à l'école."
+        s_zh = "我在海滩上。 明天我将去看医生。 昨天我在学校。"
+        s_en = "I am on the beach. Tomorrow I will go to the doctor. Yesterday I was at school."
+        s_es = "Estoy en la playa. Mañana iré al médico. Ayer estaba en la escuela."
+        s_ru = "Я на пляже. Завтра пойду к врачу. Вчера я учился в школе."
 
         # Do
         self.assertEqual(s_fr, Translator.translate_text(s_ko, from_language=Language.KOREAN, to_language=Language.FRENCH))
