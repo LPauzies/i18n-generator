a10b423825aa6b36836966fcac4fbbdca536b634
Lucas Pauzies <lucas.pauzies@capgemini.com>
2022-07-15 12:44:17 +0200

    Test with Korean instead of Chinese

--- tests_e2e/test_e2e.py
+++ tests_e2e/test_e2e.py
@@ -39,7 +39,7 @@ class TestE2E(unittest.TestCase):
 
     @clean_test_folder_at_the_end
     def test_translation_fr_to_en_zh_ru_pt_from_yaml(self):
-        locales = ["en", "zh", "ru", "pt"]
+        locales = ["en", "ko", "ru", "pt"]
         main(args=["--config", "tests_e2e/test_e2e.yaml"])
         for locale in locales:
             expected = pathlib.Path(EXPECTED_LOCALES_PATH, f"{locale}.json")
@@ -48,7 +48,7 @@ class TestE2E(unittest.TestCase):
 
     @clean_test_folder_at_the_end
     def test_translation_fr_to_en_zh_ru_pt_from_cli(self):
-        locales = ["en", "zh", "ru", "pt"]
+        locales = ["en", "ko", "ru", "pt"]
         main(args=["--main-file", str(MAIN_LOCALES_FILE), "--from-language", "fr", "--to-language", "en,zh,ru,pt"])
         for locale in locales:
             expected = pathlib.Path(EXPECTED_LOCALES_PATH, f"{locale}.json")
