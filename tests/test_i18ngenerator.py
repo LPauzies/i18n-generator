6e033cbc33ee6996fe08c9c937b937e48dad3e02
Cair0n <lucas.pauzies@hotmail.fr>
2022-07-19 12:02:24 +0200

    rewrite tests

--- tests/test_i18ngenerator.py
+++ tests/test_i18ngenerator.py
@@ -13,19 +13,22 @@ def from_json(file_path: pathlib.Path) -> Dict[str, Any]:
     with open(file_path, "r", encoding="utf-8") as f:
         return json.load(f)
 
+PATH_FR_TEST = pathlib.Path("tests/data/fr_test_case.json")
+PATH_EN_EXPECTED = pathlib.Path("tests/data/en_test_case.json")
+ITEM_COUNT = 32
+
 
 class TestI18NGenerator(unittest.TestCase):
     
     def test_generate_translation_from_dict(self):
         # Given
-        test_json = from_json(pathlib.Path("tests/data/fr_test_case.json"))
+        test_json = from_json(PATH_FR_TEST)
         i18n_generator = I18nGenerator()
         from_language = Language.FRENCH
         to_language = Language.ENGLISH
         
         # Expected
-        expected_json = from_json(pathlib.Path("tests/data/en_test_case.json"))
-        item_count = 24
+        expected_json = from_json(PATH_EN_EXPECTED)
 
         # Do
         with io.StringIO() as buffer_stdout:
@@ -34,20 +37,19 @@ class TestI18NGenerator(unittest.TestCase):
                 self.assertEqual(expected_json, tested_json)
                 self.assertEqual(expected_json.keys(), tested_json.keys())
             self.assertIn(f"Currently translating from {Language.to_locale(from_language)} to {Language.to_locale(to_language)}...", buffer_stdout.getvalue())
-            self.assertIn(f"Successfully translated {item_count} items.", buffer_stdout.getvalue())
+            self.assertIn(f"Successfully translated {ITEM_COUNT} items.", buffer_stdout.getvalue())
 
     def test__generate_translation_rec(self):
         # Given
-        test_json = from_json(pathlib.Path("tests/data/fr_test_case.json"))
+        test_json = from_json(PATH_FR_TEST)
         i18n_generator = I18nGenerator()
 
         # Expected
-        expected_json = from_json(pathlib.Path("tests/data/en_test_case.json"))
-        expected_item_count = 24
+        expected_json = from_json(PATH_EN_EXPECTED)
 
         # Do
         tested_result = {}
         tested_metadata_method = {"translated_item_count": 0}
         i18n_generator._generate_translation_rec(tested_result, test_json, Language.FRENCH, Language.ENGLISH, tested_metadata_method)
         self.assertEqual(expected_json, tested_result)
-        self.assertEqual(expected_item_count, tested_metadata_method["translated_item_count"])
+        self.assertEqual(ITEM_COUNT, tested_metadata_method["translated_item_count"])
